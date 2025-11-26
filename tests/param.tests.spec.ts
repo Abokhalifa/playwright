3670e30e1ea4dc26a171706e6766806ff18816d6
Abokhalifa <ahmed.abokhalifa@gmail.com>
2025-11-26 18:15:24 +0200

    Fix param tests.

--- tests/param.tests.spec.ts
+++ tests/param.tests.spec.ts
@@ -5,43 +5,31 @@ import { HomePage } from '../pages/HomePage';
 import { SignupForm } from '../pages/SignupForm';
 
 var landingPage:LandingPage;
-test.beforeEach(async({page})=>{
+const randomNumber = `${Math.floor(Math.random() * 10000)}`;
+
+[
+  { username: 'Alice', password: 'Hello, Alice!' },
+  { username: 'Bob', password: 'Hello, Bob!' },
+  { username: 'Charlie', password: 'Hello, Charlie!' },
+].forEach(({ username, password }) => {
+    test.beforeEach(async({page})=>{
     landingPage = new LandingPage(page);
     await landingPage.launchLandingPage();
-    })
-
-
-    const uniqueId= () => Date.now() - Math.floor(Math.random()*1000)
-test.describe(`Sign up tests.....`,()=>{
-    [
-        {
-            username: `user`+uniqueId(),
-            password: 'pass1234'
-        },
-        {
-            username: `testuser`+uniqueId(),
-            password: 'mypassword'
-        },
-        {
-            username: `alice`+uniqueId(),
-            password: 'alice123'
-        },
-    ].forEach(({username, password})=>{
-        test(`Sign up ${username}.`, async ({page})=>{
-         const signupForm:SignupForm = await landingPage.clickSignupLink();
-         await signupForm.fillinSignupForm(username,password);
-         //Listen for the dialog and accept it
-         page.on('dialog', async dialog => {
-             console.log(`Dialog message: ${dialog.message()}`);
-             expect(dialog.type()).toBe('alert');
-             expect(dialog.message()).toContain('Sign up successful.');
-         });
-
-         //Triger the action that causes the dialog to appear.
-         const homePage:HomePage = await signupForm.clickSignupButton();
-
     });
-});
+  // You can also do it with test.describe() or with multiple tests as long the test name is unique.
+  test(`Sign up ${username}`, async ({ page }) => {
+        const signupForm:SignupForm = await landingPage.clickSignupLink();
+        await signupForm.fillinSignupForm(username+randomNumber, password);
+        //Listen for the dialog and accept it
+        page.on('dialog', async dialog => {
+            console.log(`Dialog message: ${dialog.message()}`);
+            expect(dialog.type()).toBe('alert');
+            expect(dialog.message()).toContain('Sign up successful.');
+        });
+
+        //Triger the action that causes the dialog to appear.
+        const homePage:HomePage = await signupForm.clickSignupButton();
+    });
 });
 
 
